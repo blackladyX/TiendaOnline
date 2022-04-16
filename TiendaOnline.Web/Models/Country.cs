30614fdbe61994529ede8ea4189b01e10cb3f347
blackladyX <danielahinestroza@hotmail.com>
2022-04-15 23:15:59 -0500

    Inicial actualizacion

---
+++ TiendaOnline.Web/Models/Country.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Country
+{
+    public int Id { get; set; }
+    [MaxLength(50, ErrorMessage = "El campo {0} debe contener al menos un caracter")]
+    [Required]
+    public string Name { get; set; }
+}
